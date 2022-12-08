5c8608f30cbd0469c784c07c787d218acdde95c9
hwakk <x_xhwakk@naver.com>
2022-12-08 23:09:18 +0900

    feat: VideoDetail, RelatedVideos 기능 개발

--- src/components/RelatedVideos.jsx
+++ src/components/RelatedVideos.jsx
@@ -1,18 +1,16 @@
 import { useQuery } from "@tanstack/react-query";
 import React from "react";
-import { useParams } from "react-router-dom";
-import VideoCard from "../components/VideoCard";
 import { useYoutubeApi } from "../context/YoutubeApiContext";
+import VideoCard from "./VideoCard";
 
-function Videos(props) {
-  const { keyword } = useParams();
+function RelatedVideos({ id }) {
   const { youtube } = useYoutubeApi();
   const {
     isLoading,
     error,
     data: videos,
-  } = useQuery(["videos", keyword], () => youtube.search(keyword), {
-    staleTime: 1000 * 60 * 1,
+  } = useQuery(["related", id], () => youtube.relatedVideos(id), {
+    staleTime: 1000 * 60 * 5,
   });
 
   return (
@@ -20,9 +18,9 @@ function Videos(props) {
       {isLoading && <p>Loading...</p>}
       {error && <p>Something is wrong...</p>}
       {videos && (
-        <ul className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-2 gap-y-4">
+        <ul>
           {videos.map((video) => (
-            <VideoCard key={video.id} video={video} />
+            <VideoCard key={video.id} video={video} type="list" />
           ))}
         </ul>
       )}
@@ -30,4 +28,4 @@ function Videos(props) {
   );
 }
 
-export default Videos;
+export default RelatedVideos;

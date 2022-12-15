dbf845320b6936ea7ab8fbcd01925350cf96183a
hwakk <x_xhwakk@naver.com>
2022-12-15 17:23:30 +0900

    fix: App.js console.log 삭제

--- src/App.js
+++ src/App.js
@@ -7,8 +7,6 @@ import { YoutubeApiProvider } from "./context/YoutubeApiContext";
 const queryClient = new QueryClient();
 
 function App() {
-  console.log("NODE_ENV : " + process.env.NODE_ENV);
-  console.log("BASE_NAME : " + process.env.REACT_APP_BASENAME);
   return (
     <>
       <SearchHeader />
